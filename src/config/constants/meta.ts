9c57c6cc6f4b8d55256f96694e651cab6728ee5f
dmb-dev87 <dmbdev800@gmail.com>
2022-04-13 01:47:27 -0700

    [dmb-dev87][2022-04-13][chagne logo files and delete unused images]

--- src/config/constants/meta.ts
+++ src/config/constants/meta.ts
@@ -5,7 +5,7 @@ export const DEFAULT_META: PageMeta = {
   title: 'BoomerangExchange',
   description:
     'The most popular AMM on BSC by user count! Earn CAKE through yield farming or win it in the Lottery, then stake it in Syrup Pools to earn more tokens! Initial Farm Offerings (new token launch model pioneered by BoomerangExchange), NFTs, and more, on a platform you can trust.',
-  image: 'https://pancakeswap.finance/images/hero.png',
+  image: '/images/4.png',
 }
 
 export const getCustomMeta = (path: string, t: ContextApi['t']): PageMeta => {
